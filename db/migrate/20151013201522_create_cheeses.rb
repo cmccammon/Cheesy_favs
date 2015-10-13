5c42852bae7c3ed483b912e5f4eb6ca38eeb7399
Chris McCammon <cmccammon@gmail.com>
2015-10-13 16:01:16 -0500

    ran migration and added models

--- db/migrate/20151013201522_create_cheeses.rb
+++ db/migrate/20151013201522_create_cheeses.rb
@@ -1,7 +1,7 @@
 class CreateCheeses < ActiveRecord::Migration
   def change
     create_table :cheeses do |t|
-      t.string :name
+      t.string :name, null:false
 
       t.timestamps null: false
     end
